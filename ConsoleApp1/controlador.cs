baca82b74aa4dd13a4700f405927107470afdd27
Jonathan Alberto Carrera  Santamaria <jocarr01@ucm.es>
2021-04-14 20:10:35 +0200

    Se añade nueva clase

---
+++ ConsoleApp1/controlador.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class controlador
+    {
+        string texto;
+
+        public controlador(string linea)
+        {
+            this.texto = linea;
+        }
+
+        public void imprimir()
+        {
+            Console.WriteLine(texto);
+        }
+    }
+}
