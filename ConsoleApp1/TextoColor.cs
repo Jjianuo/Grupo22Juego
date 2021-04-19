9175ca3ec620d78c96ee9c2cb9f01441bd8055a5
Jianuo Wen <jwen@ucm.es>
2021-04-19 14:48:43 +0200

    Nuevo color

---
+++ ConsoleApp1/TextoColor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class TextoColor
+    {
+        string color;
+        public void EscribeTexto(string s)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(s);
+        }
+    }
+}
--- readme.txt
+++ readme.txt
@@ -1,3 +1,5 @@
 #Resumen de práctica
 
 Habituarns al ciclo de trabajo con Git
+
+Nueva funcionalidad añadida: customización de los colores del juego.
